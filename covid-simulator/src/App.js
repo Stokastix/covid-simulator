1a16f6a343e9fd366f17ffeefed5ada79dd5f46f
Antoine Bodin <antoinexp@gmail.com>
2020-11-22 10:59:13 +0100

    Chart Wrapper

--- covid-simulator/src/App.js
+++ covid-simulator/src/App.js
@@ -2,16 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import R0Slider from "./R0Slider";
+import ChartWrapper from './ChartWrapper';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <R0Slider></R0Slider>
+        <ChartWrapper />
+        <R0Slider />
       </header>
     </div>
   );
